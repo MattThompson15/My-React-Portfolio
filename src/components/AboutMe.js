4e2e1f2fcf246b0b0f5bdad3096a75bb29bad389
Matt Thompson <matthewthompson118@gmail.com>
2024-01-12 11:18:09 -0400

    created aboutme.js

---
+++ src/components/AboutMe.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <section id="about-me">
+            <h1>About Me</h1>
+            <img src="path_to_photo.jpg" alt="Me" />
+            <p>
+                I am a full stack web developer with a passion for learning and creating. I am a recent graduate of the University of New Brunswick Coding Bootcamp. I am a creative problem solver who is always looking for ways to improve my skills and learn new technologies. I am excited to start my career as a web developer and continue to grow as a developer.
+            </p>
+        </section>
+
+    );
+};
+
+export default AboutMe;
+
